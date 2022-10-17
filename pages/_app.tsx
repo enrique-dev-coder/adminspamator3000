b0bba63e20d39cb013461260f8879bb844718c3f
enrique95 <enrique_alvarado95@hotmail.com>
2022-10-17 17:21:58 -0500

    tittle and description added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>AdminSpamator3000</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Silkscreen&family=Source+Code+Pro&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
